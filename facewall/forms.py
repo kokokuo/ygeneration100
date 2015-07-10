24fc0374bf1476fcb354f665e1318e4f360d8c29
Yi-Cheng Kuo <v6610688@gmail.com>
2015-07-10 18:25:20 +0800

    Update facewall index.

--- facewall/forms.py
+++ facewall/forms.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from .models import OnlineQuestion
+# Pillow
+from PIL import Image
 import re
 
 class OnlineQuestionForm(forms.ModelForm):
@@ -20,6 +22,13 @@ class OnlineQuestionForm(forms.ModelForm):
 		name = self.cleaned_data.get('name')
 		# 特別符號，待處理
 		return name
+
+	# def clean_face_image(self):
+	# 	face_image = self.cleaned_data.get('face_image')
+	# 	image_w, image_h = face_image.size
+	# 	if image_w > 300 or image_h > 300:
+	# 		face_image.resize((300, 300), Image.ANTIALIAS)
+	# 	return face_image
 
 	def clean_youtube_url(self):
 		url = self.cleaned_data.get('youtube_url')
