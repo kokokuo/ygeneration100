0ea5ab1d43eb473980adcf46b4fd6018afaa7b8f
Yi-Cheng Kuo <v6610688@gmail.com>
2015-07-28 17:04:32 +0800

    發現原先的圖檔還是透過windows.load載入後再計算會比較穩健. 測試scroll.

--- static/js/fastquestion.js
+++ static/js/fastquestion.js
@@ -1,81 +1,6 @@
 
-// $(window).load(function () {
-// // ====================
-// 	 // jQuery masonry
-// 	$('#grid').masonry({
-// 		columnWidth: '.grid-sizer',
-// 	  // columnWidth: '.grid-sizer',
-// 	  itemSelector: '.grid-item',
-// 	  percentPosition: true,
-// 	});	
-
-// 	$('#grid-water').masonry({
-// 			columnWidth: '.grid-sizer-water',
-// 			itemSelector: '.grid-item-water',
-// 			percentPosition: true,
-// 			singleMode:true,
-// 			animate:true
-// 	});
-
-// 	// Modal ： Show Face
-// 	// need loaded images then can work
-// 	$('a[id^=face_video_]').click(function(event) {
-// 	    	event.preventDefault();
-// 	        var faceVideoModal = $('#face_modal');
-// 	        modalBody = faceVideoModal.find('.modal-body');
-
-// 			video_serial =''
-// 	        // Get id 
-// 	        var id = $(this).attr('id').split("face_video_")[1]
-// 	        console.log('face id =' + id)
-// 	        //ajax
-// 	        $.get(
-// 	        	"/fastquestion/video/",
-// 	        	{
-// 	        		face_id:id
-// 	        	},
-// 	        	function(data){
-// 			        // video serial
-// 			        video_serial = data.youtube_serial;
-// 			        console.log(id,video_serial)
-// 			        //name
-// 			        modalTitle = faceVideoModal.find('.modal-title')
-// 			        modalTitle.text(data.title)
-// 			        // load content into modal
-// 			        // ?enablejsapi=1 for enable Js Youtube api ,then can close  or pause
-// 			        content = '<div class="videowrapper">' +
-// 			                    	'<iframe width="100%" height="390px" src="https://www.youtube.com/embed/' + video_serial + '?enablejsapi=1" frameborder="0" allowfullscreen></iframe>'
-// 			                    	+
-// 			                	'</div>';
-// 			        if(modalBody.find('.videowrapper')){
-// 			        	$('.videowrapper').remove();	
-// 			        }
-// 			        // if(modalBody.find('p')){
-// 			        // 	$('p').remove();	
-// 			        // }
-// 			        modalBody.append(content);
-//         		}
-//     		);
-// 	        // display modal
-// 	        // attribute data set ,so dont need
-// 	        // faceVideoModal.modal('show');
-// 	});
-	
-// 	// detect when modal close (hidden)
-// 	$('#face_modal').on('hidden.bs.modal', function () {
-//   		// do something…
-// 		 var faceVideoModal = $('#face_modal');
-// 	    modalBody = faceVideoModal.find('.modal-body');
-
-// 	    if(modalBody.find('.videowrapper')){
-// 	    	$('.videowrapper > iframe').get(0).contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');  
-	    	
-// 		}
-// 	});
-
-// });
-
-$('#grid').imagesLoaded( function() {
+$(window).load(function () {
+// ====================
 	 // jQuery masonry
 	$('#grid').masonry({
 		columnWidth: '.grid-sizer',
@@ -83,19 +8,17 @@ $('#grid').imagesLoaded( function() {
 	  itemSelector: '.grid-item',
 	  percentPosition: true,
 	});	
-});	
 
-$('#grid-water').imagesLoaded( function() {
-	console.log('all images are loaded');
 	$('#grid-water').masonry({
-		columnWidth: '.grid-sizer-water',
-		itemSelector: '.grid-item-water',
-		percentPosition: true,
-		singleMode:true,
-		animate:true
+			columnWidth: '.grid-sizer-water',
+			itemSelector: '.grid-item-water',
+			percentPosition: true,
+			singleMode:true,
+			animate:true
 	});
 
-
+	// Modal ： Show Face
+	// need loaded images then can work
 	$('a[id^=face_video_]').click(function(event) {
 	    	event.preventDefault();
 	        var faceVideoModal = $('#face_modal');
@@ -149,9 +72,8 @@ $('#grid-water').imagesLoaded( function() {
 	    	
 		}
 	});
-});
 
-$('#grid-water').infinitescroll({
+	$('#grid-water').infinitescroll({
 
 	nextSelector: '.pagination a.next',
 	navSelector: '.pagination',
@@ -169,9 +91,88 @@ $('#grid-water').infinitescroll({
 			$newElems.imagesLoaded(function(){
 				// show elems now they're ready
 				$newElems.animate({ opacity: 1 });
-				$container.masonry( 'appended', $newElems, true );
-			});
+				$('#grid-water').masonry( 'appended', $newElems, true );
+		});
 
 });
+});
+
+// $('#grid').imagesLoaded( function() {
+// 	 // jQuery masonry
+// 	$('#grid').masonry({
+// 		columnWidth: '.grid-sizer',
+// 	  // columnWidth: '.grid-sizer',
+// 	  itemSelector: '.grid-item',
+// 	  percentPosition: true,
+// 	});	
+// });	
+
+// $('#grid-water').imagesLoaded( function() {
+// 	console.log('all images are loaded');
+// 	$('#grid-water').masonry({
+// 		columnWidth: '.grid-sizer-water',
+// 		itemSelector: '.grid-item-water',
+// 		percentPosition: true,
+// 		singleMode:true,
+// 		animate:true
+// 	});
+
+
+// 	$('a[id^=face_video_]').click(function(event) {
+// 	    	event.preventDefault();
+// 	        var faceVideoModal = $('#face_modal');
+// 	        modalBody = faceVideoModal.find('.modal-body');
+
+// 			video_serial =''
+// 	        // Get id 
+// 	        var id = $(this).attr('id').split("face_video_")[1]
+// 	        console.log('face id =' + id)
+// 	        //ajax
+// 	        $.get(
+// 	        	"/fastquestion/video/",
+// 	        	{
+// 	        		face_id:id
+// 	        	},
+// 	        	function(data){
+// 			        // video serial
+// 			        video_serial = data.youtube_serial;
+// 			        console.log(id,video_serial)
+// 			        //name
+// 			        modalTitle = faceVideoModal.find('.modal-title')
+// 			        modalTitle.text(data.title)
+// 			        // load content into modal
+// 			        // ?enablejsapi=1 for enable Js Youtube api ,then can close  or pause
+// 			        content = '<div class="videowrapper">' +
+// 			                    	'<iframe width="100%" height="390px" src="https://www.youtube.com/embed/' + video_serial + '?enablejsapi=1" frameborder="0" allowfullscreen></iframe>'
+// 			                    	+
+// 			                	'</div>';
+// 			        if(modalBody.find('.videowrapper')){
+// 			        	$('.videowrapper').remove();	
+// 			        }
+// 			        // if(modalBody.find('p')){
+// 			        // 	$('p').remove();	
+// 			        // }
+// 			        modalBody.append(content);
+//         		}
+//     		);
+// 	        // display modal
+// 	        // attribute data set ,so dont need
+// 	        // faceVideoModal.modal('show');
+// 	});
+	
+// 	// detect when modal close (hidden)
+// 	$('#face_modal').on('hidden.bs.modal', function () {
+//   		// do something…
+// 		 var faceVideoModal = $('#face_modal');
+// 	    modalBody = faceVideoModal.find('.modal-body');
+
+// 	    if(modalBody.find('.videowrapper')){
+// 	    	$('.videowrapper > iframe').get(0).contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');  
+	    	
+// 		}
+// 	});
+// });
+
+
 
 
