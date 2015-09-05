42618de438457bc3baadcdd5de84f9b02c7062e0
Yi-Cheng Kuo <v6610688@gmail.com>
2015-09-05 11:17:51 +0800

    update fastquestion/index.html url template and url dispatcher.

--- fastquestion/urls.py
+++ fastquestion/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns(
 	url(r'^index/$', 'fastquestion.views.index', name="fastquestion_wall"),
 	url(r'^video/$', 'fastquestion.views.get_face_video'),
 
-	url(r'^index/(?P<page_id>\d)/$', 'fastquestion.views.get_page'),
+	url(r'^index/page/(?P<page_id>\d)/$', 'fastquestion.views.get_page', name="fastquestion_page"),
 )
